675469fd1f6c1b6ad219269f8993ced81540b163
werlito <wesleygg23@gmail.com>
2022-07-28 14:52:41 -0400

    Feature: componentizar o itemcard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import styled, {createGlobalStyle} from 'styled-components';
+import image from './images/gumball.jpg'
 
 const GlobalStyle = createGlobalStyle`
   *{
@@ -22,9 +23,11 @@ const Caixa = styled.div `
   padding-top: 30px;
   border-radius: 25px; 
   margin: 0 auto;
+  margin-botton: 10px;
   width: 1000px;
   height: 250px;
   background-image: linear-gradient(to left, rgb(252, 217, 65), rgb(253, 245, 170));
+
 `
 const Caixafoto = styled.img `
 float: right;
@@ -35,18 +38,74 @@ margin-top: -68px;
 margin-right: 50px;
 `
 
+const ItemCard = ({titulo, descricao, image, alt}) =>{
 
+  return (
+    <Container>
+    <Caixa>
+        <h1>{titulo}</h1>
+        <p>{descricao}</p>
+        <Caixafoto src={image} alt= {alt}/>
+    </Caixa>
+  </Container>
+  )
+}
 function App() {
+
+  const ListaTarefa = [
+    {
+      titulo:"Gumball",
+      descricao:"fundo amarelo",
+      image:"https://www.petz.com.br/blog/wp-content/uploads/2021/11/enxoval-para-gato-Copia.jpg",
+      alt:"gumball num fundo amarelo"
+  
+    },
+    {
+      titulo:"wes",
+      descricao:"fundo amarelo",
+      image:"https://www.petz.com.br/blog/wp-content/uploads/2021/11/enxoval-para-gato-Copia.jpg",
+      alt:"wes num fundo amarelo"
+  
+    },
+    {
+      titulo:"isinha",
+      descricao:"fundo amarelo",
+      image:"https://www.petz.com.br/blog/wp-content/uploads/2021/11/enxoval-para-gato-Copia.jpg",
+      alt:"isainha num fundo amarelo"
+  
+    },
+    {
+      titulo:"Gumball",
+      descricao:"fundo amarelo",
+      image:"https://www.petz.com.br/blog/wp-content/uploads/2021/11/enxoval-para-gato-Copia.jpg",
+      alt:"gumball num fundo amarelo"
+  
+    },
+    {
+      titulo:"wes",
+      descricao:"fundo amarelo",
+      image:"https://www.petz.com.br/blog/wp-content/uploads/2021/11/enxoval-para-gato-Copia.jpg",
+      alt:"wes num fundo amarelo"
+  
+    },
+    {
+      titulo:"isinha",
+      descricao:"fundo amarelo",
+      image:"https://www.petz.com.br/blog/wp-content/uploads/2021/11/enxoval-para-gato-Copia.jpg",
+      alt:"isainha num fundo amarelo"
+  
+    }
+
+  ]
   return (
     <div>
       <GlobalStyle/>
-      <Container>
-        <Caixa>
-            <h1>sitezin</h1>
-            <p>site bala</p>
-            <Caixafoto src="tile.jpg" alt="gumball num fundo amarelo"/>
-        </Caixa>
-      </Container>
+      
+      {
+        ListaTarefa.map((tarefa) => (
+          <ItemCard {...tarefa}/>
+        ))
+      }
     </div>
   )
 }
